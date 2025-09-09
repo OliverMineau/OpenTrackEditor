116a4f0f9f210ad377637d1a47bb49bcd81e09fc
OliverMineau <35004220+OliverMineau@users.noreply.github.com>
2025-09-09 16:53:18 +0200

    error

---
+++ app/src/main/java/com/minapps/trackeditor/feature_track_import/di/ImportTrackModule.kt
@@ -0,0 +1,18 @@
+package com.minapps.trackeditor.feature_track_import.di
+
+import com.minapps.trackeditor.feature_track_import.data.repository.TrackImportRepositoryImpl
+import com.minapps.trackeditor.core.domain.repository.TrackImportRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class ImportTrackModule {
+
+    @Binds
+    abstract fun bindTrackImportRepository(
+        impl: TrackImportRepositoryImpl
+    ): TrackImportRepository
+}
